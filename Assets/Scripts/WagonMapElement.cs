d099a9bafc68962cd1f157b24ab6f2b92e76e6af
PuKoren <koren@pukogames.com>
2014-06-14 19:38:05 +0200

    Wagons prefab, working scene, minimap

---
+++ Assets/Scripts/WagonMapElement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WagonMapElement : MonoBehaviour {
+    private int wagonID = 0;
+    public Transform mapElement = null;
+	// Use this for initialization
+	void Start () {
+	    foreach(var a in this.name.Split('-')){
+            int id = 0;
+            if(int.TryParse(a, out id)){
+                wagonID = id;
+                if (mapElement){
+                    Instantiate(mapElement, new Vector3(0.055f * id, 0f, 0f), Quaternion.identity);
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/WagonMoves.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WagonMoves : MonoBehaviour {
+    
+    private float lastMove;
+    private Vector2 startPosition;
+    private bool fallback = false;
+    private Vector2 curMoveSpeed = Vector2.zero;
+
+    public float moveInterval = 1f;
+    public float moveMaxVelocity = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        lastMove = Time.realtimeSinceStartup;
+        startPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lastMove + moveInterval < Time.realtimeSinceStartup){
+            if (curMoveSpeed == Vector2.zero){
+                lastMove = Time.realtimeSinceStartup;
+                curMoveSpeed.x = Random.Range(-moveMaxVelocity, moveMaxVelocity);
+                curMoveSpeed.y = Random.Range(0.01f, moveMaxVelocity);
+            }
+            else if(fallback){
+                fallback = false;
+                curMoveSpeed = Vector2.zero;
+                this.transform.position = startPosition;
+            }
+            else{
+                fallback = true;
+                curMoveSpeed = -curMoveSpeed;
+                lastMove = Time.realtimeSinceStartup;
+            }
+        }
+        this.transform.position += new Vector3(curMoveSpeed.x * Time.deltaTime, curMoveSpeed.y * Time.deltaTime, 0f);
+	}
+}
